9f025c327363ad32cc96c1676a75ebf4bd73c705
mnreeves <natsir.productions@gmail.com>
2023-02-18 22:32:21 +0700

    init project

---
+++ src/Memoir.js
@@ -0,0 +1,36 @@
+import TimelineItem from '@mui/lab/TimelineItem';
+import TimelineSeparator from '@mui/lab/TimelineSeparator';
+import TimelineConnector from '@mui/lab/TimelineConnector';
+import TimelineContent from '@mui/lab/TimelineContent';
+import TimelineOppositeContent from '@mui/lab/TimelineOppositeContent';
+import TimelineDot from '@mui/lab/TimelineDot';
+import FastfoodIcon from '@mui/icons-material/Fastfood';
+import Typography from '@mui/material/Typography';
+
+export default function Memoir() {
+  return (
+    <TimelineItem>
+      <TimelineOppositeContent
+        sx={{ m: 'auto 0' }}
+        align="right"
+        variant="body2"
+        color="text.secondary"
+      >
+        9:30 AM
+      </TimelineOppositeContent>
+      <TimelineSeparator>
+        <TimelineConnector />
+        <TimelineDot>
+          <FastfoodIcon />
+        </TimelineDot>
+        <TimelineConnector />
+      </TimelineSeparator>
+      <TimelineContent sx={{ py: '12px', px: 2 }}>
+        <Typography variant="h6" component="span">
+          Eat
+        </Typography>
+        <Typography>Because you need strength</Typography>
+      </TimelineContent>
+    </TimelineItem>
+  )
+}
